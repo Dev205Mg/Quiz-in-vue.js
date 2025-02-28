30c932a623ab26a313f793db8ad03b9df5bfe895
Dev205 <dev205mg@gmail.com>
2025-02-28 11:41:56 +0300

    question fini

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,7 +2,11 @@
   <div>
     <h1>{{ quiz.title }}</h1>
     <Progress :value="step" :max="quiz.questions.length - 1"/>
-    <Question :question="question" />
+    <Question 
+      :question="question" 
+      v-if="state === 'question'"
+      @answer="addAnswer"/>
+    {{ answers }}
   </div>
 </template>
 
@@ -15,7 +19,17 @@ const props = defineProps({
   quiz: Object
 })
 
+const state = ref('question')
+const answers = ref(props.quiz.questions.map(() => null))
 const step = ref(0)
-
 const question = computed(() => props.quiz.questions[step.value])
+const addAnswer = (answer) =>  {
+  answers.value[step.value] = answer
+  if(step.value === props.quiz.questions.length - 1){
+    state.value = 'recap'
+  }else{
+    step.value++
+  }
+  
+}
 </script>
