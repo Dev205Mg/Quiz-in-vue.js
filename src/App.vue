179d8333aee19af161177c706f33d8ac7e197f2b
Dev205 <dev205mg@gmail.com>
2025-02-28 10:38:11 +0300

    component quiz

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <div>
-    {{ quiz }}
+  <div class="container">
+    <div v-if="state === 'error'">
+      <p>
+        impossible de charger le quiz
+      </p>
+    </div>
+    <div :aria-busy="state === 'loading'">
+      <Quiz :quiz="quiz" v-if="quiz" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import Quiz from './components/Quiz.vue';
 
 const quiz = ref(null)
+const state = ref('loading')
 
 onMounted(() => {
   fetch('/quiz.json')
     .then(r => {
-      if(r.ok){
+      if (r.ok) {
         return r.json()
       }
       throw new Error('Impossible de recuperer le json')
     })
     .then(data => {
       quiz.value = data
+      state.value = 'idle'
+    })
+    .catch(e => {
+      state.value = 'error'
     })
 })
 </script>
+
+<style>
+.container{
+  margin-top: 2rem;
+}
+</style>
