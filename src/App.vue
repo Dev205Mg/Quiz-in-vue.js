21035670f0823fe96e8bc1dc8b55cd196aa348ea
Dev205 <dev205mg@gmail.com>
2025-02-28 10:08:53 +0300

    fetch quiz.json

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  
+  <div>
+    {{ quiz }}
+  </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
 
+const quiz = ref(null)
+
+onMounted(() => {
+  fetch('/quiz.json')
+    .then(r => {
+      if(r.ok){
+        return r.json()
+      }
+      throw new Error('Impossible de recuperer le json')
+    })
+    .then(data => {
+      quiz.value = data
+    })
+})
 </script>
